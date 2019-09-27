b45d3a7f00fa873ecf6e5d2878d80c5e362ede7a
jimmyaudrio <jimmyaudrio16111999@gmail.com>
2019-09-27 15:42:17 +0700

    Membuat red_fragment dan blue_fragment menjadi satu

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,12 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void handleClickDynamicFragment(View view) {
+
+    }
+
+    public void handleClickStaticFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleClickPraktikumFragment(View view) {
     }
 }
