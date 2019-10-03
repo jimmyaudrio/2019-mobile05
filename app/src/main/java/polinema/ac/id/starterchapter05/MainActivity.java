91ef0e2d40ff1459514090f4c8b92b3f19432258
jimmyaudrio <jimmyaudrio16111999@gmail.com>
2019-10-03 23:57:09 +0700

    SELESAI

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import polinema.ac.id.starterchapter05.activities.DynamicActivity;
 import polinema.ac.id.starterchapter05.activities.StaticActivity;
+import polinema.ac.id.starterchapter05.activities.TugasActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void handleClickPraktikumFragment(View view) {
 
-        Intent intent = new Intent(this, StaticActivity.class);
+    public void handleClickTugasFragment(View view) {
+        Intent intent = new Intent(this, TugasActivity.class);
         startActivity(intent);
     }
 }
